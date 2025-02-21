3d8d62a8c637194fa9d3536bd8d4944a6a3dd5c2
Luiz Fernando <aulas-programacao@bol.com.br>
2025-02-20 21:33:02 -0300

    jogo1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('./sw.js')
         .then(() => {
             console.log("Service worker registrado!")
         })
